fa24205898acf6ea89703cb45d4da69b9ffbf9c8
yumichael <michael.yang2357@gmail.com>
2016-11-13 00:16:48 -0500

    non-debug runserver

--- runserver.py
+++ runserver.py
@@ -6,9 +6,10 @@ from os import environ
 from blackboard import app
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT)
+    #HOST = environ.get('SERVER_HOST', 'localhost')
+    #try:
+    #    PORT = int(environ.get('SERVER_PORT', '5555'))
+    #except ValueError:
+    #    PORT = 5555
+    #app.run(HOST, PORT)
+    app.run()
