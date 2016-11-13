7b4821fb899810860bc728cbf3441e0eca041c3c
yumichael <michael.yang2357@gmail.com>
2016-11-13 18:19:56 -0500

    add ping functionality to keep dyno running on heroku free tier

--- blackboard/views.py
+++ blackboard/views.py
@@ -4,7 +4,7 @@ Routes and views for the flask application.
 
 import os
 from datetime import datetime
-from flask import render_template
+from flask import render_template, jsonify
 from blackboard import app
 from blackboard.model import Model
 from jinja2 import Environment
@@ -50,3 +50,7 @@ def source():
         dev_name=os.environ['DEV_NAME'],
         year=datetime.now().year,
     )
+
+@app.route('/ping')
+def pong():
+    return jsonify(pong=1)
