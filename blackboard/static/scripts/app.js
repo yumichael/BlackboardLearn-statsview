5c705c298e4e119dd6974cca7952f2edee994fa8
yumichael <michael.yang2357@gmail.com>
2016-11-29 03:39:27 -0500

    added placeholder for failed plot update

--- blackboard/static/scripts/app.js
+++ blackboard/static/scripts/app.js
@@ -5,23 +5,23 @@ var metric = 'Quiz 1';
 var group = 'Tut 01';
 var plotID = 'the-plot';
 var plotData = [{
-    x: grades[metric][group],
     type: 'histogram',
+    x: [],
     xbins: {
-        start: -totalPoints[metric] / 40,
-        end: totalPoints[metric] * 1.025,
-        size: totalPoints[metric] / 20
+        start: 0,
+        end: 1,
+        size: 1
     },
     autobinx: false,
     hoverinfo: 'skip'
 }];
 var layout = {
-    title: metric + ', ' + group,
+    title: 'Choose a section and a test',
     xaxis: {
         title: 'Grade',
-        range: [-totalPoints[metric] / 40, totalPoints[metric] * 1.025],
+        range: [0, 1],
         tick0: 0,
-        dtick: totalPoints[metric] / 10
+        dtick: 1
     },
     yaxis: {
         range: [0, 18]
@@ -31,19 +31,19 @@ var layout = {
             'color': '#0099FF', 'dash': 'solid', 'width': 1
         },
         'type': 'line',
-        'x0': mean(grades[metric][group]),
-        'x1': mean(grades[metric][group]),
+        'x0': 0,
+        'x1': 0,
         'xref': 'x',
         'y0': -0.09,
         'y1': 1,
         'yref': 'paper'
     }],
     annotations: [{
-        x: mean(grades[metric][group]),
+        x: 0,
         y: -.1,
         xref: 'x',
         yref: 'paper',
-        text: "mean = " + mean(grades[metric][group]).toFixed(1),
+        text: '',
         showarrow: true,
         arrowhead: 7,
         ax: 1,
@@ -58,6 +58,14 @@ function makePlot() {
 }
 
 function refreshPlot() {
+    layout.title = 'Trying to update... perhaps this data is missing';
+    layout.xaxis.range = [0, 1];
+    plotData[0].x = [];
+    plotData[0].xbins.end = 1;
+    plotData[0].xbins.size = 1;
+    layout.xaxis.dtick = 1;
+    Plotly.redraw(plotID);
+
     layout.title = metric + ', ' + group;
     layout.xaxis.range = [-totalPoints[metric] / 40, totalPoints[metric] * 1.025];
     plotData[0].x = grades[metric][group];
@@ -68,8 +76,6 @@ function refreshPlot() {
     layout.shapes[0].x1 = mean(grades[metric][group]);
     layout.annotations[0].x = mean(grades[metric][group]);
     layout.annotations[0].text = "Mean = " + mean(grades[metric][group]).toFixed(1);
-    //Plotly.purge(plotID);
-    //$('#' + plotID).empty();
     Plotly.redraw(plotID);
 }
 
@@ -77,6 +83,7 @@ $(document).ready(function () {
     makePlot();
     $(".click-metric[value='" + metric + "']").addClass('active');
     $(".click-group[value='" + group + "']").addClass('active');
+    refreshPlot();
     $('.click-metric').click(function () {
         $('.click-metric.active').removeClass('active');
         $(this).addClass('active');
